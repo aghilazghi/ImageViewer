ea25b732a03285afd55fc1197ba6f2ab3dd89da6
Amine Ghilazghi <aghilazghi@yahoo.com>
2017-06-14 13:28:36 -0400

    Initial Commit

---
+++ PictureViewer/PictureViewer.Designer.cs
@@ -0,0 +1,171 @@
+﻿namespace PictureViewer
+{
+    partial class FrmPictureviewer
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.btnLoadPicture = new System.Windows.Forms.Button();
+            this.btnRotate = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.tableLayoutPanel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.flowLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 15F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 85F));
+            this.tableLayoutPanel1.Controls.Add(this.pictureBox1, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.checkBox1, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.flowLayoutPanel1, 1, 1);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 2;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 85F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 15F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(584, 361);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // pictureBox1
+            // 
+            this.tableLayoutPanel1.SetColumnSpan(this.pictureBox1, 2);
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox1.Location = new System.Drawing.Point(3, 3);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(578, 300);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(3, 309);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(60, 17);
+            this.checkBox1.TabIndex = 1;
+            this.checkBox1.Text = "Stretch";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.Controls.Add(this.btnLoadPicture);
+            this.flowLayoutPanel1.Controls.Add(this.btnRotate);
+            this.flowLayoutPanel1.Controls.Add(this.btnClear);
+            this.flowLayoutPanel1.Controls.Add(this.btnClose);
+            this.flowLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(90, 309);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(491, 49);
+            this.flowLayoutPanel1.TabIndex = 2;
+            // 
+            // btnLoadPicture
+            // 
+            this.btnLoadPicture.Location = new System.Drawing.Point(10, 5);
+            this.btnLoadPicture.Margin = new System.Windows.Forms.Padding(10, 5, 10, 5);
+            this.btnLoadPicture.Name = "btnLoadPicture";
+            this.btnLoadPicture.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.btnLoadPicture.Size = new System.Drawing.Size(100, 25);
+            this.btnLoadPicture.TabIndex = 0;
+            this.btnLoadPicture.Text = "Load Picture";
+            this.btnLoadPicture.UseVisualStyleBackColor = true;
+            this.btnLoadPicture.Click += new System.EventHandler(this.btnLoadPicture_Click);
+            // 
+            // btnRotate
+            // 
+            this.btnRotate.Location = new System.Drawing.Point(130, 5);
+            this.btnRotate.Margin = new System.Windows.Forms.Padding(10, 5, 10, 5);
+            this.btnRotate.Name = "btnRotate";
+            this.btnRotate.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.btnRotate.Size = new System.Drawing.Size(100, 25);
+            this.btnRotate.TabIndex = 1;
+            this.btnRotate.Text = "Rotate";
+            this.btnRotate.UseVisualStyleBackColor = true;
+            this.btnRotate.Click += new System.EventHandler(this.btnRotate_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(250, 5);
+            this.btnClear.Margin = new System.Windows.Forms.Padding(10, 5, 10, 5);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.btnClear.Size = new System.Drawing.Size(100, 25);
+            this.btnClear.TabIndex = 2;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // btnClose
+            // 
+            this.btnClose.Location = new System.Drawing.Point(370, 5);
+            this.btnClose.Margin = new System.Windows.Forms.Padding(10, 5, 10, 5);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.btnClose.Size = new System.Drawing.Size(100, 25);
+            this.btnClose.TabIndex = 3;
+            this.btnClose.Text = "Close";
+            this.btnClose.UseVisualStyleBackColor = true;
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
+            // 
+            // frmPictureviewer
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(584, 361);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "FrmPictureviewer";
+            this.Text = "Picture Viewer";
+            this.Load += new System.EventHandler(this.frmPictureViewer_Load);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.flowLayoutPanel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.Button btnLoadPicture;
+        private System.Windows.Forms.Button btnRotate;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button btnClose;
+    }
+}
+
---
+++ PictureViewer/PictureViewer.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Windows.Forms;
+
+namespace PictureViewer
+{
+    public partial class FrmPictureviewer : Form
+    {
+        private bool _isImageLoaded;
+
+        public FrmPictureviewer()
+        {
+            InitializeComponent();
+        }
+
+        private void frmPictureViewer_Load(object sender, EventArgs e)
+        {
+            pictureBox1.BackColor = Color.Bisque;
+        }
+
+        private void btnLoadPicture_Click(object sender, EventArgs e)
+        {
+
+            var dialog = new OpenFileDialog
+            {
+                Filter =
+                    @"JPEG Files (*.jpeg)|*.jpeg|PNG Files (*.png)|*.png|JPG Files (*.jpg)|*.jpg|GIF Files (*.gif)|*.gif",
+                Title = @"Select an Image File"
+            };
+
+            if (dialog.ShowDialog() != DialogResult.OK) return;
+
+            pictureBox1.Load(dialog.FileName);
+            _isImageLoaded = true;
+        }
+
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            pictureBox1.Image = null;
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            pictureBox1.SizeMode = checkBox1.Checked ? 
+                                    PictureBoxSizeMode.StretchImage : 
+                                    PictureBoxSizeMode.Normal;
+        }
+
+        private void btnRotate_Click(object sender, EventArgs e)
+        {
+            if (!_isImageLoaded) return;
+
+            var bitmap = (Bitmap) pictureBox1.Image;
+            pictureBox1.Image = RotateImage(bitmap, 90.0f);
+        }
+
+        private static Bitmap RotateImage(Image bmp, float angle)
+        {
+            var w = bmp.Width;
+            var h = bmp.Height;
+            var tempImage = new Bitmap(w, h);
+            var graphic = Graphics.FromImage(tempImage);
+
+            graphic.DrawImageUnscaled(bmp, 1, 1);
+            graphic.Dispose();
+
+            var path = new GraphicsPath();
+            path.AddRectangle(new RectangleF(0f, 0f, w, h));
+            var matrix = new Matrix();
+            matrix.Rotate(angle);
+
+            var rectangle = path.GetBounds(matrix);
+            var newImage = new Bitmap(Convert.ToInt32(rectangle.Width), Convert.ToInt32(rectangle.Height));
+
+            graphic = Graphics.FromImage(newImage);
+            graphic.TranslateTransform(-rectangle.X, -rectangle.Y);
+            graphic.RotateTransform(angle);
+            graphic.InterpolationMode = InterpolationMode.HighQualityBilinear;
+            graphic.DrawImageUnscaled(tempImage, 0, 0);
+            graphic.Dispose();
+            tempImage.Dispose();
+
+            return newImage;
+        }
+
+       
+    }
+}
